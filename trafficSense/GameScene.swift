125f8eee7d0d78ad2e13beb6f646993efe563567
Yousef <yousefahmed0066@gmail.com>
2019-06-18 15:36:49 -0500

    initial

---
+++ trafficSense/GameScene.swift
@@ -0,0 +1,168 @@
+//
+//  GameScene.swift
+//  trafficSense
+//
+//  Created by Mohamed Ahmed on 6/13/19.
+//  Copyright © 2019 Yousef Ahmed. All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    
+    var entities = [GKEntity]()
+    var graphs = [String : GKGraph]()
+    
+    private var lastUpdateTime : TimeInterval = 0
+    private var label : SKLabelNode?
+    private var car : SKShapeNode?
+    private var spinnyNode : SKShapeNode?
+    private var light = SKShapeNode(circleOfRadius: 100)
+    private var line = SKShapeNode(rect: CGRect(x: 0, y: 300, width: 10, height: 200))
+    
+    override func sceneDidLoad() {
+
+        self.lastUpdateTime = 0
+        
+        // Get label node from scene and store it for use later
+        self.car = self.childNode(withName: "//car") as? SKShapeNode
+        if let car = self.car {
+            car.fillColor = SKColor.blue
+        }
+        
+        line.position.x = -self.frame.width/2 + 70
+        // How to cure cancer:
+//        let shape = SKShapeNode()
+//        shape.path = UIBezierPath(arcCenter: CGPoint(x: 100, y:0), radius: 100, startAngle: 0, endAngle: 2*3.1415, clockwise: true).cgPath
+// //       shape.position = CGPoint(x: frame.midX, y: frame.midY)
+//        shape.fillColor = UIColor.red
+//        addChild(shape)
+        
+        light.position = CGPoint(x: -200, y: 300)
+        light.fillColor = SKColor.red
+        light.name = "light"
+        light.isUserInteractionEnabled = false
+        self.addChild(light)
+        
+        line.fillColor = SKColor.white
+        self.addChild(line)
+//        // Create shape node to use during mouse interaction
+//        let w = (self.size.width + self.size.height) * 0.05
+//        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
+//
+//        if let spinnyNode = self.spinnyNode {
+//            spinnyNode.lineWidth = 2.5
+//
+//            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
+//            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
+//                                              SKAction.fadeOut(withDuration: 0.5),
+//                                              SKAction.removeFromParent()]))
+//        }
+    }
+    
+    
+//    func touchDown(atPoint pos : CGPoint) {
+//        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
+//            n.position = pos
+//            n.strokeColor = SKColor.green
+//            self.addChild(n)
+//        }
+//    }
+//
+//    func touchMoved(toPoint pos : CGPoint) {
+//        if let car = self.car {
+//            car.position = pos
+//        }
+//    }
+//
+//    func touchUp(atPoint pos : CGPoint) {
+//        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
+//            n.position = pos
+//            n.strokeColor = SKColor.red
+//            self.addChild(n)
+//        }
+//    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if let label = self.label {
+            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
+        }
+        
+    //    for t in touches { self.touchDown(atPoint: t.location(in: self)) }
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+    //    for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for touch: AnyObject in touches{
+            
+            let pointOfTouch = touch.location(in: self)
+            let nodeITapped = atPoint(pointOfTouch)
+            let nameOfTappedNode = nodeITapped.name
+            
+            if nameOfTappedNode == "light"{
+                //make it do whatever you want
+                switchLight()
+                
+            }
+            
+        }
+
+    }
+    
+    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
+    //    for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+    }
+    
+    func switchLight() {
+        if (light.fillColor == SKColor.red){
+            light.fillColor = SKColor.green
+        }
+        else {
+            light.fillColor = SKColor.red
+        }
+    }
+    
+    func move() {
+        if let car = self.car {
+            car.position.x -= 1
+        }
+        if (car!.position.x <= -self.scene!.size.width/2) {
+            print(car!.position.x)
+            car!.position.x = self.scene!.size.width/2
+            print(car!.position.x)
+        }
+        
+    }
+    
+    func checkLight() -> Bool {
+        return light.fillColor == SKColor.green
+    }
+    
+    override func update(_ currentTime: TimeInterval) {
+        if (checkLight()){
+            move()
+        }
+        
+        
+        // Called before each frame is rendered
+        
+        // Initialize _lastUpdateTime if it has not already been
+        if (self.lastUpdateTime == 0) {
+            self.lastUpdateTime = currentTime
+        }
+        
+        // Calculate time since last update
+        let dt = currentTime - self.lastUpdateTime
+        
+        // Update entities
+        for entity in self.entities {
+            entity.update(deltaTime: dt)
+        }
+        
+        self.lastUpdateTime = currentTime
+    }
+}
